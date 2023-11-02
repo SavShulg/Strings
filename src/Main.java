8129b18d95d3bf402c0b16a3c105835d39a9a827
Home <zavalnyy_saveliy@mail.ru>
2023-11-02 17:34:40 +1000

    Strings 1-1-2 complited

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // ЗАДАЧИ
+
+        // Задача 1
+
+        String firstName = " Sveta ";
+        String middleName = " Skyprovich ";
+        String lastName = " Chibrikova ";
+        String fullName = firstName + middleName + lastName;
+
+        System.out.println("Ф. И. О. сотрудника —" + fullName);
+
+        // Задача 2
+
+//        Либо String fullName1 = firstName + middleName + lastName; и менять содержание выше.
+        String fullName1 = " Ivanov " + " Ivan " + " Ivanovich "; // <--- либо здесь менять содержание строки здесь.
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName1.toUpperCase());
+
+        // 3 Задача
+
+        String fullName2 = "Иванов " + " Семён " + " Ёжикович ";
+        fullName2 = fullName2.replace("ё", "е");
+        fullName2 = fullName2.replace("Ё", "Е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName2);
+
+
     }
 }
