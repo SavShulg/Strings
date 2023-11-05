4773f47987b3edd87a15f04d0d5ee816172aba22
Home <zavalnyy_saveliy@mail.ru>
2023-11-05 22:43:52 +1000

    Strings 1-1-3 complited

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
 
+
+
         // ЗАДАЧИ
 
         // Задача 1
@@ -16,7 +18,7 @@ public class Main {
         // Задача 2
 
 //        Либо String fullName1 = firstName + middleName + lastName; и менять содержание выше.
-        String fullName1 = " Ivanov " + " Ivan " + " Ivanovich "; // <--- либо здесь менять содержание строки здесь.
+        String fullName1 = " Ivanov " + " Ivan " + " Ivanovich "; // <--- либо здесь менять содержание строки 
         System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName1.toUpperCase());
 
         // 3 Задача
@@ -26,6 +28,62 @@ public class Main {
         fullName2 = fullName2.replace("Ё", "Е");
         System.out.println("Данные Ф. И. О. сотрудника — " + fullName2);
 
+        // 2 Часть задач
 
+        // 1 Задача
+
+
+
+
+        isLeapYear(1323113);
+        isLeapYear(942313);
+        isLeapYear(682987913);
+        isLeapYear(1872904513);
+
+        printAppVersion(745, 1);
+
+        var days = calculateDeliveryTime(37);
+        if (days == -1) {
+            System.out.println("Доставки нет!");
+        } else {
+            System.out.println("Количество дней: " + days);
+        }
+
+
+    }
+    private static void isLeapYear(int year) {
+        if (year > 1584 && (year % 4 == 0 && 100 != 0 || year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+    }
+
+    private static void printAppVersion(int deviceYear, int clientOS) {
+        if (deviceYear < 2015 && clientOS ==0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (deviceYear < 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (deviceYear > 2015 && clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (deviceYear > 2015 && clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+    private static int calculateDeliveryTime(int distance) {
+        if (distance > 100) {
+            return  -1;
+
+        } else {
+            int days = 1;
+            if (distance> 20) {
+                days++;
+            }
+            if (distance > 60) {
+                days++;
+            }
+            return days;
+        }
     }
 }
